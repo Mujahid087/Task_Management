b51323c52060e69afc9354278936677359e249b4
Mujahid087 <137553605+Mujahid087@users.noreply.github.com>
2025-01-11 22:11:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 const app=express()
 
 app.use(cors({
-    origin:["https://mernstack-management.netlify.app"],
+    origin:["https://mernstack-management.netlify.app","‎http://localhost:3001","‎http://localhost:3000"],
     methods:["GET","POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials:true,
